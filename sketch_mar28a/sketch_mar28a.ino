6899adf5ac71497cf69e58c09aec7970edb405c1
LinardsN <linardsn@gmail.com>
2025-04-01 15:34:10 +0300

    test

--- sketch_mar28a/sketch_mar28a.ino
+++ sketch_mar28a/sketch_mar28a.ino
@@ -22,7 +22,7 @@ const unsigned long cooldown1 = 15 * 60;
 const unsigned long cooldown2 = 10 * 60;
 const unsigned long cooldown3 = 30 * 60;
 
-String firmwareVersion = "v20250401-1529";
+String firmwareVersion = "v20250401-1533";
 String messages[200];
 int messageCount = 0;
 bool isWiFiReady = false;
@@ -46,7 +46,7 @@ void setup() {
   if (wokeFromButton) {
     log("🔘 Wake from deep sleep by button press.");
 
-    delay(1000);
+    delay(400);
 
     // Start press counting immediately
     unsigned long pressStart = millis();
